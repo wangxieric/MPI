d3d4390df46046e26d7213b65bfc27cabdd6ca9c
Xi Wang <wangxieric@gmail.com>
2025-09-12 13:18:23 +0100

    Update vary_personality.py

--- models/vary_personality.py
+++ models/vary_personality.py
@@ -147,7 +147,7 @@ def main():
         Change this to test different traits
         Available choices: Openness, Conscientiousness, Extraversion, Agreeableness, Neuroticism
     '''
-    enhanced_trait = "Extraversion"
+    enhanced_trait = "Agreeableness"
     result, count = generateAnswer(tokenizer, model, dataset, enhanced_trait, template)
 
     mean_var = calc_mean_and_var(result)
